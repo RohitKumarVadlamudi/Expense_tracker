d67dae1fe62ff4b4cd59b8c5fb592d83f0a4e8f8
RohitKumarVadlamudi <98195539+RohitKumarVadlamudi@users.noreply.github.com>
2025-05-21 05:38:58 -0400

    Update app.py
    Created a route for home and connected database so it can show all the transactions in the table.

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import sqlite3 as sq
 
 app = Flask(__name__)
+
 
 @app.route("/")
 
 def home():
-	return
+	conn = sq.connect("expenses.db")
+	cursor=conn.cursor()
+	cursor.execute("""SELECT*FROM expenses""")
+	expenses=cursor.fetchall()
+	conn.close()
+	return render_template("home.html", expenses=expenses)
 
 if __name__=="__main__":
 	app.run(debug=True)
