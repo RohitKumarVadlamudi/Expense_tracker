416bd15abc28d664c32bbbe97de42f9eb0129448
RohitKumarVadlamudi <98195539+RohitKumarVadlamudi@users.noreply.github.com>
2025-05-22 22:17:44 -0400

    Month, year selection
    Made query to extract month and year and then use .get() to match it with the selections. Operational with the drop down menu.

--- app.py
+++ app.py
@@ -9,9 +9,15 @@ app = Flask(__name__)
 def home():
 	conn = sq.connect("expenses.db")
 	cursor=conn.cursor()
+
 	cursor.execute("""SELECT DISTINCT strftime('%m', date), strftime('%Y', date) FROM expenses""")
 	month_year=cursor.fetchall()
-	cursor.execute("""SELECT*FROM expenses""")
+	selected_month=request.args.get("month")
+	selected_year=request.args.get("year")
+	if selected_month and selected_month:
+		cursor.execute("""SELECT*FROM expenses WHERE strftime('%m', date) = ? AND strftime('%Y', date) = ?""", (selected_month,selected_year))
+	else:
+		cursor.execute("""SELECT*FROM expenses""")
 	expenses=cursor.fetchall()
 	conn.close()
 	return render_template("home.html", month=[item[0] for item in month_year], year= [item[1] for item in month_year], expenses=expenses)
